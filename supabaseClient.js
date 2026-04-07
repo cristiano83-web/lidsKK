01d6a2423daceaee046dae2bd7e3714adeb27f5c
cristiano83-web <paes.cristiano@escola.pr.gov.br>
2026-04-07 09:17:31 -0300

    dsfrdsfgdstgfd

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,5 +4,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
   'https://egqgimenkefjmmtysatj.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVncWdpbWVua2Vmam1tdHlzYXRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MTk0NTEsImV4cCI6MjA4NjI5NTQ1MX0.faRdHlFIyDxHy7lQIq74YkuLBuKdKodAMQYcqws2jCU'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVncWdpbWVua2Vmam1tdHlzYXRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MTk0NTEsImV4cCI6MjA4NjI5NTQ1MX0.faRdHlFIyDxHy7lQIq74YkuLBuKdKodAMQYcqws2jCU',
   
